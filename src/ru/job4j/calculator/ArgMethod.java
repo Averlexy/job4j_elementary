57194191eb15d24a9147bb8521c2bd4d25a09711
Averlexy <averlexy@yandex.ru>
2021-08-26 22:32:57 +0300

    9.2. Checkstyle [#4123]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -1,10 +1,11 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello(String name, int age ){
-        System.out.println("Hello," + name + ", age = "+ age );
+    public static void hello(String name, int age) {
+        System.out.println("Hello," + name + ", age = " + age);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String name = "job4j";
         int age = 33;
         ArgMethod.hello(name, age);
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,17 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value){
-        float rsl = value/70;
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
         return rsl;
     }
-    public static float rubleToDollar(float value){
-        float rsl = value/60;
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
         return rsl;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         float euro = rubleToEuro(140);
         float dollar = rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro");
