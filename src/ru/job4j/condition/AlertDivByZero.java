5c8dbe8dfb3970497dc0c28a48abd221941eeaa9
Averlexy <averlexy@yandex.ru>
2021-08-31 22:59:22 +0300

    4.1. Операторы ветвлений [#3611]

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+    }
+
+    public static void possibleDiv(int namber) {
+        if (namber < 0) {
+            System.out.println("This is negative numbers ");
+        }
+    }
+}
--- src/ru/job4j/condition/Liken.java
+++ src/ru/job4j/condition/Liken.java
@@ -15,3 +15,5 @@ public class Liken {
                 System.out.println(result);
     }
 }
+
+
